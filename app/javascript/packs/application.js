93a6dc5da4c3c0a3a2f3f90bd855e00d2acce82c
Daniel Sperling <daniel_sperling@yahoo.com>
2020-11-04 20:46:24 +0200

    added parallax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("./products")
 require("./about")
 // require("./map")
+require("./parallax.min")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

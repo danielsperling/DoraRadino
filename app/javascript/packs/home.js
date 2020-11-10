c9e5b964e2cc8ee7bdd1011673acbab74cce1626
Daniel Sperling <daniel_sperling@yahoo.com>
2020-11-10 18:53:52 +0200

    done

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -61,3 +61,22 @@ const buildPicture = (data) => {
 }
 
 
+// const italian = document.getElementById("italian");
+// const translation = document.getElementById('home-information');
+
+// function setUpEventHandlers() {
+//     italian.addEventListener('click', (event) => {
+//         updateUI(event.target.value);
+//     }, true);
+// }
+
+// function updateUI(locale = 'en-US') {
+//     client.getEntries({
+//         'sys.id': '1tObOSALaX8lCVmhQZlDKk',
+//         locale: locale
+//     }).then(response => {
+//         const entry = responce.items[0];
+//         entry.map(toHtml);
+
+//     });
+// }

f819d766bf375a2a2788830677bda976ae52bac0
Daniel Sperling <daniel_sperling@yahoo.com>
2021-01-07 17:39:40 +0100

    add translation yml and translations on html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en-US|it/ do
-
-  root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'products', to: 'pages#products'
-  get 'contact', to: 'pages#contact'
-  get 'gallery', to: 'pages#gallery'
+  scope '(:locale)', locale: /en-US|it/ do
+    root to: 'pages#home'
+    get 'about', to: 'pages#about'
+    get 'products', to: 'pages#products'
+    get 'contact', to: 'pages#contact'
+    get 'gallery', to: 'pages#gallery'
+  end
 end
